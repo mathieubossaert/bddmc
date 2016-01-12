515c05bcfaf11aef3f85e5e38a881960d877a767
mathieubossaert <mathieu.bossaert@cenlr.org>
2016-01-12 11:23:32 +0100

    Update vues_reporting.sql

--- vues_reporting.sql
+++ vues_reporting.sql
@@ -1,6 +1,11 @@
 CREATE SCHEMA reporting;
 
 CREATE VIEW reporting.nb_ha_convention_mae AS
-SELECT count(id_ug) as nb_ug, count(id_contrat) as nb_contrats, st_area2d(st_union(geometrie))::numeric/10000 as surf_tota_ha
+SELECT now()::date as date, count(id_ug) as nb_ug, count(id_contrat) as nb_contrats, st_area2d(st_union(geometrie))::numeric/10000 as surf_tota_ha
   FROM compensation.unite_de_gestion JOIN compensation.contrat_de_gestion USING(id_contrat)
+  WHERE daterange(date_effet, date_expiration, '[]') @>now()::date;
+  
+CREATE VIEW reporting.nb_exploitants_sous_convention AS
+SELECT now()::date AS date, count(id_exploitant) as nb_exploitants, count(id_contrat) as nb_contrats
+  FROM compensation.contrat_de_gestion
   WHERE daterange(date_effet, date_expiration, '[]') @>now()::date;
