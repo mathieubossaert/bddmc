5fd22dc3228107c3f8c5370c8f20c1a05b04b8d0
mathieubossaert <mathieu.bossaert@cenlr.org>
2016-01-12 11:26:17 +0100

    Update vues_reporting.sql

--- vues_reporting.sql
+++ vues_reporting.sql
@@ -9,3 +9,9 @@ CREATE VIEW reporting.nb_exploitants_sous_convention AS
 SELECT now()::date AS date, count(id_exploitant) as nb_exploitants, count(id_contrat) as nb_contrats
   FROM compensation.contrat_de_gestion
   WHERE daterange(date_effet, date_expiration, '[]') @>now()::date;
+
+CREATE VIEW reporting.nb_ha_en_convention_pare_code_mesure AS
+SELECT now()::date as date, code_mesure, st_area2d(st_union(geometrie))::numeric/10000 as surf_tota_ha
+  FROM compensation.unite_de_gestion JOIN compensation.contrat_de_gestion USING(id_contrat)
+  WHERE daterange(date_effet, date_expiration, '[]') @>now()::date
+  GROUP By code_mesure;
