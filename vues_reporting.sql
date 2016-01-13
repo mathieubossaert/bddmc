01185800686e1002bf6b944d0b98efad40aaff4e
mathieubossaert <mathieu.bossaert@cenlr.org>
2016-01-13 14:28:20 +0100

    Update vues_reporting.sql

--- vues_reporting.sql
+++ vues_reporting.sql
@@ -19,14 +19,16 @@ SELECT now()::date as date, code_mesure, st_area2d(st_union(geometrie))::numeric
 CREATE VIEW reporting.cout_total_euros_mae_signees AS
 SELECT now()::date as date, sum(indemnite_exploitant_euros) as cout_total_mae_signees
   FROM compensation.unite_de_gestion JOIN compensation.contrat_de_gestion USING(id_contrat)
-  WHERE date_effet<=now()::date
+  WHERE date_effet<=now()::date;
 
-CREATE VIEW reporting.cout_moyen_ha_par_exploitant AS
-SELECT now()::date as date, raison_sociale, avg(indemnite_exploitant_euros_ha) as cout_moyen_ha
-  FROM compensation.unite_de_gestion JOIN compensation.contrat_de_gestion USING(id_contrat)
-  JOIN compensation.exploitant USING(id_exploitant)
-  WHERE date_effet<=now()::date
-  GROUP BY raison_sociale
+CREATE VIEW reporting.cout_moyen_ha_par_exploitant AS 
+ SELECT now()::date AS date, exploitant.raison_sociale, 
+    round(avg(unite_de_gestion.indemnite_exploitant_euros_ha),2) AS cout_moyen_ha
+   FROM compensation.unite_de_gestion
+   JOIN compensation.contrat_de_gestion USING (id_contrat)
+   JOIN compensation.exploitant USING (id_exploitant)
+  WHERE COALESCE(contrat_de_gestion.date_effet,contrat_de_gestion.date_signature) <= now()::date
+  GROUP BY exploitant.raison_sociale;
 
 CREATE VIEW reporting.nb_moyen_contrat_par_exploitant AS
 SELECT now()::date as date, count(distinct id_contrat) as nb_contrats, count(distinct id_exploitant) as nb_exploitant, CASE WHEN count(id_exploitant)>0 THEN count(distinct id_contrat)::numeric/count(distinct id_exploitant)::numeric ELSE NULL END as nb_moyen_contrat_par_exploitant
